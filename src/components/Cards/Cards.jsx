7a99d8abb82a54ae4c5e9b7adb865dff3f2d0895
Shreya Rajput <shreyaaraj1996@gmail.com>
2020-05-03 10:25:01 -0700

    line chart is completed

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -87,7 +87,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
             <Typography color="textSecondary">
               {new Date(lastUpdate).toDateString()}
             </Typography>
-            <Typography variant="body">Number of active of COVID-19</Typography>
+            <Typography variant="body">Number of deaths from COVID-19</Typography>
           </CardContent>
         </Grid>
       </Grid>
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+
+import styles from "./CountryPicker.module.css";
+
+import { fetchCountries } from "../../api";
 
 const CountryPicker = () => {
-  return <h1>Cards</h1>;
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries);
+    };
+    fetchAPI();
+  }, [setFetchedCountries]);
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect>
+        <option value="global">Global</option>
+        {fetchedCountries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
 };
 export default CountryPicker;
